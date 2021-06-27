8ee2c0e52067c1665f37cfbcba0b8ba3adb5aed2
HoBee <lzqpublic@163.com>
2021-06-27 18:05:27 +0800

    Add files via upload

---
+++ openipynb.py
@@ -0,0 +1,22 @@
+import json
+import pickle
+
+with open("Untitled.ipynb",'r') as f:
+    data_str = ""
+    for i in f.readlines():
+        data_str += i
+    data = json.loads(data_str)
+
+cells = data["cells"]
+source_code = ""
+for cell in cells:
+    if cell["cell_type"] == "code":
+        count = cell["execution_count"]
+        if not count:
+            continue
+        source_code += f"\n## In[{count}]\n"
+        for code in cell["source"]:
+            source_code += code
+        source_code += '\n'
+
+print(source_code)
