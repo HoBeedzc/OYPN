53aab5175f568091d881f28ce0bea2dc7a7d69de
hobee_mac <lzqpublic@163.com>
2021-07-08 00:34:04 +0800

    init OpenIpynb CLI Tools

---
+++ OpenIpynb-CLI.py
@@ -0,0 +1,25 @@
+import json
+import pickle
+import sys
+
+filepath = sys.argv[1]
+
+with open(filepath,'r') as f:
+    data_str = ""
+    for i in f.readlines():
+        data_str += i
+    data = json.loads(data_str)
+
+cells = data["cells"]
+source_code = ""
+for cell in cells:
+    if cell["cell_type"] == "code":
+        count = cell["execution_count"]
+        if not count:
+            continue
+        source_code += f"\n## In[{count}]\n"
+        for code in cell["source"]:
+            source_code += code
+        source_code += '\n'
+
+print(source_code)
